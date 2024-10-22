c80be52cc661f77a83413a7a55fdb88c4de80305
CrisHale <67316386+CrisHale@users.noreply.github.com>
2024-10-22 19:39:50 +0100

    Upload remaining changed files

---
+++ keyboards/dm9records/plaid/keymaps/CrisHale/config.h
@@ -0,0 +1,26 @@
+/* Copyright 2019 Takuya Urakawa (dm9records.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+// place overrides here
+
+//#define ONESHOT_TIMEOUT 2000  /* Time (in ms) before the one shot key is released */
+#define TAPPING_TERM 200
+#define TAPPING_TOGGLE 2
+#define LEADER_TIMEOUT 750
+#define LEADER_PER_KEY_TIMING
+#define TAP_CODE_DELAY 50
---
+++ keyboards/dm9records/plaid/keymaps/CrisHale/rules.mk
@@ -0,0 +1,1 @@
+LEADER_ENABLE = yes # Must be disabled to use the keyboard with the BIOS (this comment was probably about older BIOSes)
